8ddd829d1f02109b18e4bf6d17e1b05b31e367cc
arasopraza <aras.opraza@gmail.com>
2024-05-04 18:48:33 +0700

    feat: add dto and response struct

--- models/dto/cat_dto.go
+++ models/dto/cat_dto.go
@@ -4,3 +4,12 @@ type SaveCatDto struct {
 	Id        string `json:"id"`
 	CreatedAt string `json:"createdAt"`
 }
+
+type MatchCatsDto struct {
+	Id         string `json:"id"`
+	MatchCatId string `json:"matchCatDetail"`
+	UserCatId  string `json:"userCatDetail"`
+	IssuedBy   string `json:"issuedBy"`
+	Message    string `json:"message"`
+	CreatedAt  string `json:"createdAt"`
+}
--- models/response/cat.go
+++ models/response/cat.go
@@ -11,3 +11,30 @@ type GetCats struct {
 	HasMatched  bool     `json:"hasMatched"`
 	CreatedAt   string   `json:"createdAt"`
 }
+
+type GetMatchs struct {
+	Id             string    `json:"id"`
+	IssuedBy       IssuedBy  `json:"issuedBy"`
+	MatchCatDetail CatDetail `json:"matchCatDetail"`
+	UserCatDetail  CatDetail `json:"userCatDetail"`
+	Message        string    `json:"message"`
+	CreatedAt      string    `json:"createdAt"`
+}
+
+type IssuedBy struct {
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"createdAt"`
+}
+
+type CatDetail struct {
+	Id          string   `json:"id"`
+	Name        string   `json:"name"`
+	Race        string   `json:"race"`
+	Sex         string   `json:"sex"`
+	Description string   `json:"description"`
+	AgeInMonth  int      `json:"ageInMonth"`
+	ImageUrls   []string `json:"imageUrls"`
+	HasMatched  bool     `json:"hasMatched"`
+	CreatedAt   string   `json:"createdAt"`
+}
